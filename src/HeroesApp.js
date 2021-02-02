12ac267c82ff58afbf63e25f25d46814279d2b30
Andrés Arana Castillo <aac.devs@gmail.com>
2021-02-02 01:07:43 -0300

    components structure and  routes created

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,9 +1,8 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Navbar } from "./components/ui/Navbar";
+import { AppRouter } from "./routers/AppRouter";
 
 export const HeroesApp = () => {
-  return (
-    <div>
-      <h1>Heroes App</h1>
-    </div>
-  );
+  return <AppRouter />;
 };
